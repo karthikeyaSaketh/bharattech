c00d28ba0cbb5fb51f36b2dad6fb6c7053ff183b
karthikeyaSaketh <sakethvadali28@gmail.com>
2024-05-17 18:59:05 +0530

    width

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -6,23 +6,23 @@ const ContactUs = () => {
       <div className='flex w-full sm:w-[95%] h-[700px] sm:h-[760px]' style={{ boxShadow: '0px 0px 20px rgba(0,0,0, 0.25)' }}>
         <div className='flex flex-col items-center w-[380px] sm:w-[500px] lg:w-[700px] text-[#0077B5] font-medium'>
           <h1 className='mt-12 lg:ml-20 text-5xl sm:text-7xl lg:text-[84px]'>Get in touch</h1>
-          <div className='mt-8 flex flex-col items-start'>
+          <div className='mt-8 flex flex-col items-start pl-5 sm:pl-0'>
             <form>
               <label htmlFor='name' >Name :</label>
               <input id="name" type="text" placeholder='  Enter your name'
-                className="appearance-none border-2 border-gray-300 rounded-xl w-full py-3 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-gray-400 font-normal" />
+                className="appearance-none border-2 border-gray-300 rounded-xl w-[90%] sm:w-full py-3 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-gray-400 font-normal" />
               <br />
               <label htmlFor='email' >Email :</label>
               <input id="email" type="text" placeholder='  Enter a valid email address'
-                className="appearance-none border-2 border-gray-300 rounded-xl w-full py-3 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-gray-400 font-normal" />
+                className="appearance-none border-2 border-gray-300 rounded-xl w-[90%] sm:w-full py-3 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-gray-400 font-normal" />
                 <br/>
               <label htmlFor='message' >Message :</label>
               <textarea id="message" type="text" placeholder='  Enter your message'
-                className="appearance-none border-2 border-gray-300 rounded-xl w-full py-3 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-gray-400 font-normal"
+                className="appearance-none border-2 border-gray-300 rounded-xl w-[90%] sm:w-full py-3 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-gray-400 font-normal"
                 rows="6"/>
                 <br/>
                 <button type='submit' 
-                  className="rounded-xl w-full py-4 px-3 my-4 leading-tight font-normal text-white"
+                  className="rounded-xl w-[90%] sm:w-full py-4 px-3 my-4 leading-tight font-normal text-white"
                   style={{ background: "linear-gradient(90deg, rgba(0, 119, 181, 1) 0%, rgba(0, 189, 232, 1) 100%)"}}
                   >
                   Submit your response
