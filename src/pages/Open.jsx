82403c9c2da084b2ea6f907650fce7c0540ddcc3
karthikeyaSaketh <sakethvadali28@gmail.com>
2024-07-04 23:46:19 +0530

    open

--- src/pages/Open.jsx
+++ src/pages/Open.jsx
@@ -3,7 +3,7 @@ import Footer from '../Components/Footer';
 
 const Open = () => {
   const calculateTimeLeft = () => {
-    const difference = +new Date('2024-06-30T00:00:00') - +new Date();
+    const difference = +new Date('2024-07-06T00:00:00') - +new Date();
     let timeLeft = {};
 
     if (difference > 0) {
