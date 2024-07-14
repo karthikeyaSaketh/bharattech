c119309bc0c9305b4a26c689b0db2a233f9ebb84
karthikeyaSaketh <sakethvadali28@gmail.com>
2024-07-14 18:55:53 +0530

    faqs

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -78,10 +78,8 @@ const ContactUs = () => {
   };
 
   return (
-    <div className='m-4 sm:m-24 lg:mt-8 lg:mb-4 flex justify-center content-center'>
+    <div className='m-4 sm:m-24 lg:mt-8 lg:mb-4 flex flex-col justify-center items-center'>
       <div className='flex flex-col align-center items-center lg:flex-row w-full xl:w-[80%]' style={{ boxShadow: '0px 0px 20px rgba(0,0,0, 0.25)' }}>
-
-
         <div className='flex flex-col w-[80%] lg:w-[50%] justify-center items-center pt-12 lg:pt-0 px-0 sm:px-10 xl:px-20 gap-12'>
           <h1 className='text-[32px] sm:text-[40px] text-[#1B8534] font-bold'>
             Connect With Us
@@ -152,6 +150,295 @@ const ContactUs = () => {
               </button>
             </form>
           </div>
+        </div>
+
+
+      </div>
+
+
+      <div className='flex flex-col gap-6 w-full mt-4'>
+        <h1 className='text-[22px] lg:text-[32px] font-bold mt-10 mb-6 text-center'>Frequently Asked Questions (FAQs) for Bharat Teck League – 2024</h1>
+
+        <div className='flex flex-col items-start justify-center w-[95%] gap-6 leading-8 text-justify'>
+          <p className='text-[22px] font-bold pl-4'>1. Who can participate in the Bharat Teck League – 2024 ?</p>
+          <ul className="list-none space-y-4 pl-4 sm:pl-16 w-full">
+            <li className="flex">
+              <span className="mr-4">-</span>
+              <span>
+                Junior Category: Students from 6th to 10th grade.
+              </span>
+            </li>
+            <li className="flex">
+              <span className="mr-4">-</span>
+              <span>
+                Senior Category: Diploma, Degree and Engineering students (any year).
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <div className='flex flex-col items-start justify-center w-[95%] gap-6 leading-8 text-justify'>
+          <p className='text-[22px] font-bold pl-4'>2. What is the main theme of the Bharat Teck League – 2024 ?</p>
+          <ul className="list-none space-y-4 pl-4 sm:pl-16 w-full">
+            <li className="flex">
+              <span className="mr-4">-</span>
+              <span>
+                The main theme is agriculture, focusing on the role of innovation and technology in this field.
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <div className='flex flex-col items-start justify-center w-[95%] gap-6 leading-8 text-justify'>
+          <p className='text-[22px] font-bold pl-4'>3. Is there a registration fee ?</p>
+          <ul className="list-none space-y-4 pl-4 sm:pl-16 w-full">
+            <li className="flex">
+              <span className="mr-4">-</span>
+              <span>
+                Yes, registration fees are as follows:
+              </span>
+            </li>
+            <li className="flex">
+              <span className="mr-4">-</span>
+              <span>
+                Zonal Level: Rs. 349/- per head for Juniors & Rs. 449/- per head for Seniors (Selected participants only).
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <div className='flex flex-col items-start justify-center w-[95%] gap-6 leading-8 text-justify'>
+          <p className='text-[22px] font-bold pl-4'>4. How many levels are there in this event ?</p>
+          <ul className="list-none space-y-4 pl-4 sm:pl-16 w-full">
+            <li className="flex">
+              <span className="mr-4">-</span>
+              <span>
+                The Bharat Teck League – 2024 consists of three levels:
+              </span>
+            </li>
+            <li className="flex">
+              <span className="mr-4">-</span>
+              <span>
+                Level 1: Prelims (online participation)
+              </span>
+            </li>
+            <li className="flex">
+              <span className="mr-4">-</span>
+              <span>
+                Level 2: Zonal (physical participation)
+              </span>
+            </li><li className="flex">
+              <span className="mr-4">-</span>
+              <span>
+                Level 3: Finals (physical participation)
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <div className='flex flex-col items-start justify-center w-[95%] gap-6 leading-8 text-justify'>
+          <p className='text-[22px] font-bold pl-4'>5. Is participation individual or in groups ?</p>
+          <ul className="list-none space-y-4 pl-4 sm:pl-16 w-full">
+            <li className="flex">
+              <span className="mr-4">-</span>
+              <span>
+                Participation is for groups only. Each team must consist of either 2 or 4 members. Teams with 1 or 3 members are not allowed.
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <div className='flex flex-col items-start justify-center w-[95%] gap-6 leading-8 text-justify'>
+          <p className='text-[22px] font-bold pl-4'>6. What is the registration process ?</p>
+          <ul className="list-none space-y-4 pl-4 sm:pl-16 w-full">
+            <li className="flex">
+              <span className="mr-4">-</span>
+              <span>
+                Register through the website using the following links:
+              </span>
+            </li>
+            <li className="flex flex-row text-wrap">
+              <span className="mr-4">-</span>
+              <span className="sm:flex-1">
+                Juniors : <a href="https://www.bharatteckleague.com/bharattech/Registration/Juniors" className="underline text-blue-600" target='_blank'>Junior Registration</a>
+              </span>
+            </li>
+            <li className="flex flex-row text-wrap mt-4 sm:mt-0">
+              <span className="mr-4">-</span>
+              <span className="sm:flex-1">
+                Seniors : <a href="https://www.bharatteckleague.com/bharattech/Registration/Seniors" className="underline text-blue-600" target='_blank'>Senior Registration</a>
+              </span>
+            </li>
+
+            <li className="flex">
+              <span className="mr-4">-</span>
+              <span>
+                Each team must also submit an event-related abstract/video via email to bharatteckleague@gmail.com. Abstracts can include direct video, drive links, or YouTube links. Please refer to the problem statements for details.
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <div className='flex flex-col items-start justify-center w-[95%] gap-6 leading-8 text-justify'>
+          <p className='text-[22px] font-bold pl-4'>7. Is a mentor mandatory for junior participants ?</p>
+          <ul className="list-none space-y-4 pl-4 sm:pl-16 w-full">
+            <li className="flex">
+              <span className="mr-4">-</span>
+              <span>
+                Yes, a mentor is mandatory for junior participants.
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <div className='flex flex-col items-start justify-center w-[95%] gap-6 leading-8 text-justify'>
+          <p className='text-[22px] font-bold pl-4'>8. Is there a specific format and duration for the abstract ?</p>
+          <ul className="list-none space-y-4 pl-4 sm:pl-16 w-full">
+            <li className="flex">
+              <span className="mr-4">-</span>
+              <span>
+                Please refer to the problem statement for complete details specific to each competition.
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <div className='flex flex-col items-start justify-center w-[95%] gap-6 leading-8 text-justify'>
+          <p className='text-[22px] font-bold pl-4'>9. What is the last date for registration and abstract submission ?</p>
+          <ul className="list-none space-y-4 pl-4 sm:pl-16 w-full">
+            <li className="flex">
+              <span className="mr-4">-</span>
+              <span>
+                Last date for registrations and abstract/video submission is 28-07-2024. Please check the website regularly for updates.
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <div className='flex flex-col items-start justify-center w-[95%] gap-6 leading-8 text-justify'>
+          <p className='text-[22px] font-bold pl-4'>10. Will the organizers provide travel and accommodation for zonal and finals participants ?</p>
+          <ul className="list-none space-y-4 pl-4 sm:pl-16 w-full">
+            <li className="flex">
+              <span className="mr-4">-</span>
+              <span>
+                No, the organizers will not provide travel and accommodation for participants. Participants should plan their own travel and accommodation.
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <div className='flex flex-col items-start justify-center w-[95%] gap-6 leading-8 text-justify'>
+          <p className='text-[22px] font-bold pl-4'>11. What are the competition categories ?</p>
+          <ul className="list-none space-y-4 pl-4 sm:pl-16 w-full">
+            <li className="flex">
+              <span className="mr-4">-</span>
+              <span>
+                Junior Level: 3D Printing, Robotics, Drones, and Clean Energy.
+              </span>
+            </li>
+            <li className="flex">
+              <span className="mr-4">-</span>
+              <span>
+                Senior Level: Artificial Intelligence, Robotics, Drones, and Internet of Things (IoT).
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <div className='flex flex-col items-start justify-center w-[95%] gap-6 leading-8 text-justify'>
+          <p className='text-[22px] font-bold pl-4'>12. Can we modify our team after registration ?</p>
+          <ul className="list-none space-y-4 pl-4 sm:pl-16 w-full">
+            <li className="flex">
+              <span className="mr-4">-</span>
+              <span>
+                No, once a team is registered, the members must remain the same throughout all levels of the competition.
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <div className='flex flex-col items-start justify-center w-[95%] gap-6 leading-8 text-justify'>
+          <p className='text-[22px] font-bold pl-4'>13. Where can I find the problem statements ?</p>
+          <ul className="list-none space-y-4 pl-4 sm:pl-16 w-full">
+            <li className="flex">
+              <span className="mr-4">-</span>
+              <span>
+                Problem statements are available on the Bharat Teck League website - Events page.
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <div className='flex flex-col items-start justify-center w-[95%] gap-6 leading-8 text-justify'>
+          <p className='text-[22px] font-bold pl-4'>14. What criteria are used for judging the performance/projects ?</p>
+          <ul className="list-none space-y-4 pl-4 sm:pl-16 w-full">
+            <li className="flex">
+              <span className="mr-4">-</span>
+              <span>
+                Performance / Projects are judged based on time, innovation, functionality, presentation, and relevance to the agriculture theme.
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <div className='flex flex-col items-start justify-center w-[95%] gap-6 leading-8 text-justify'>
+          <p className='text-[22px] font-bold pl-4'>15. How will we be notified of competition updates ?</p>
+          <ul className="list-none space-y-4 pl-4 sm:pl-16 w-full">
+            <li className="flex">
+              <span className="mr-4">-</span>
+              <span>
+                All updates will be posted on the Bharat Teck League website, communicated via email and WhatsApp community.
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <div className='flex flex-col items-start justify-center w-[95%] gap-6 leading-8 text-justify'>
+          <p className='text-[22px] font-bold pl-4'>16. Can participants use their own internet during the competition ?</p>
+          <ul className="list-none space-y-4 pl-4 sm:pl-16 w-full">
+            <li className="flex">
+              <span className="mr-4">-</span>
+              <span>
+                Yes, participants can use their own internet access if needed to refer to previous work, online resources, and documentation during the competition. However, browsing unrelated websites, seeking external help, or using communication platforms during the competition is prohibited.
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <div className='flex flex-col items-start justify-center w-[95%] gap-6 leading-8 text-justify'>
+          <p className='text-[22px] font-bold pl-4'>17. What should participants bring to the competition venue ?</p>
+          <ul className="list-none space-y-4 pl-4 sm:pl-16 w-full">
+            <li className="flex">
+              <span className="mr-4">-</span>
+              <span>
+                Participants must bring their own laptops, laptop chargers, and any other necessary tools and equipment. Ensure all required software and libraries are pre-installed.
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <div className='flex flex-col items-start justify-center w-[95%] gap-6 leading-8 text-justify'>
+          <p className='text-[22px] font-bold pl-4'>18. Is there any support available for participants during the competition ?</p>
+          <ul className="list-none space-y-4 pl-4 sm:pl-16 w-full">
+            <li className="flex">
+              <span className="mr-4">-</span>
+              <span>
+                Limited support will be available on-site, but participants are expected to be self-sufficient and prepared with all necessary resources.
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <div className='flex flex-col items-start justify-center w-[95%] gap-6 leading-8 text-justify mb-20'>
+          <p className='text-[22px] font-bold pl-4'>19. Are there any prizes for the winners ?</p>
+          <ul className="list-none space-y-4 pl-4 sm:pl-16 w-full">
+            <li className="flex">
+              <span className="mr-4">-</span>
+              <span>
+                Yes, there are prizes and certificates for winners at each level. Details will be announced on the website soon.
+              </span>
+            </li>
+          </ul>
         </div>
 
 
